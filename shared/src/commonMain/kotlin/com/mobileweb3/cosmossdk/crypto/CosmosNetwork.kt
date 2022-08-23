f1147774fb514e2785cff30b8aa479431722e68e
Dmitriy Kuzmin <hook23@mail.ru>
2022-08-23 06:47:01 +0300

    shared: fix generating address for osmosis

--- shared/src/commonMain/kotlin/com/mobileweb3/cosmossdk/crypto/CosmosNetwork.kt
+++ shared/src/commonMain/kotlin/com/mobileweb3/cosmossdk/crypto/CosmosNetwork.kt
@@ -9,6 +9,7 @@ sealed class CosmosNetwork(
     val displayName: String,
     val rawNetworkName: String,
     val toBytesName: String,
+    val mintScanReferencePart: String,
     val childNetworkNumber: Int,
     val HDDerivationPath: String
 ) {
@@ -21,6 +22,7 @@ sealed class CosmosNetwork(
         displayName = "Cosmos",
         rawNetworkName = "cosmoshub-mainnet",
         toBytesName = "cosmos",
+        mintScanReferencePart = "cosmos",
         childNetworkNumber = 118,
         HDDerivationPath = "m/44'/118'/0'/0/"
     )
@@ -28,7 +30,8 @@ sealed class CosmosNetwork(
     object Osmosis : CosmosNetwork(
         displayName = "Osmosis",
         rawNetworkName = "osmosis-mainnet",
-        toBytesName = "osmosis",
+        toBytesName = "osmo",
+        mintScanReferencePart = "osmosis",
         childNetworkNumber = 118,
         HDDerivationPath = "m/44'/118'/0'/0/"
     )
